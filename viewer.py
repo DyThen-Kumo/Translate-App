886c89992c491e5f023483baf26ece1a5e090f2a
DuyThang-std <nguyenduythang864pp@gmail.com>
2024-01-19 23:24:58 +0700

    update some funtion

--- viewer.py
+++ viewer.py
@@ -1,11 +1,4 @@
-from tkinter import *
-from tkinter import ttk
-from PIL import Image, ImageTk
-import googletrans
-
-## Use print(googletrans.LANGUAGES) to see more language supported (in another program) or run file language.py.
-dic = {'Vietnamese':'vi', 'English':'en', 'Japanese':'ja'}
-trans = googletrans.Translator()
+from controller import *
 
 ## TK window
 root = Tk()
@@ -25,38 +18,32 @@ name.config(font=('Courier',30,'bold'))
 name.pack(padx=10, pady=10)
 
 ## Set language
-lang_in = ttk.Combobox(root, values=["Vietnamese", "English", "Japanese"], background="#FEEBE7", width=12)
+lang_in = ttk.Combobox(root, values=languages_value, background="#FEEBE7", width=12)
 lang_in.place(relx=0.3, y=75, anchor='center')
-lang_in.set('Vietnamese')
-lang_out = ttk.Combobox(root, values=["Vietnamese", "English", "Japanese"], background="#FEEBE7", width=12)
+lang_in.set('vietnamese')
+lang_out = ttk.Combobox(root, values=languages_value, background="#FEEBE7", width=12)
 lang_out.place(relx=0.7, y=75, anchor='center')
-lang_out.set('English')
+lang_out.set('english')
 
 ## Input
 box_in = Text(root, width=250, height=7, font=('Arial',15), background='#FEEBE7')
 box_in.pack(padx=50, pady=30)
 
-## Button bellow
-button_frame = Frame(root).pack(side='bottom')
-def clear():
-    box_in.delete(1.0,END)
-    box_out.delete(1.0,END)
-
-def translate():
-    box_out.delete(1.0,END)
-    input = box_in.get(1.0, END)
-    temp = trans.translate(input, src=dic[lang_in.get()], dest=dic[lang_out.get()])
-    output = temp.text
-    box_out.insert(END, output)
-
-clear_button = Button(button_frame, text='CLEAR', font=('ROBOTO',10), bg='#030303', fg='#FFFFFF', command=clear)
-clear_button.place(relx=0.6, y=300, anchor='center')
-trans_button = Button(button_frame, text='TRANSLATE', font=('ROBOTO',10), bg='#030303', fg='#FFFFFF',command=translate)
-trans_button.place(relx=0.4, y=300, anchor='center')
-
 ## Output
 box_out = Text(root, width=250, height=7, font=('Arial',15), background="#FEEBE7")
 box_out.pack(padx=50, pady=50)
+
+## Button bellow
+button_frame = Frame(root).pack(side='bottom')
+
+clear_button = Button(button_frame, text='CLEAR', font=('ROBOTO',10), bg='#030303', fg='#FFFFFF')
+clear_button.config(command=lambda: clear(box_in=box_in, box_out=box_out))
+clear_button.place(relx=0.6, y=300, anchor='center')
+
+trans_button = Button(button_frame, text='TRANSLATE', font=('ROBOTO',10), bg='#030303', fg='#FFFFFF')
+trans_button.config(command= lambda: translate(box_in=box_in, box_out=box_out, lang_in=languages[lang_in.get()], lang_out=languages[lang_out.get()]))
+trans_button.place(relx=0.4, y=300, anchor='center')
+
 
 ## Focus
 focus = Label(root, text="*App will take a few seconds.", fg="#000000", bd=0, background="#FEEBE7")
