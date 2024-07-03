ee7162375a2d18e0ceeb6345ca29542d0800a159
DuyThang-std <nguyenduythang864pp@gmail.com>
2024-07-04 00:50:44 +0700

    Add ocr system base on Transformer for English

--- controller.py
+++ controller.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 import tkinter as tk
 from PIL import Image, ImageTk
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 import googletrans
 
 ## Get all languages which is supported
@@ -13,6 +15,10 @@ for key, value in languages_origin.items():
 
 ## Translator
 trans = googletrans.Translator()
+## ORC Predict
+# Khởi tạo mô hình và processor từ Hugging Face
+processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
+model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten')
 
 def clear(box_in, box_out):
     box_in.delete(1.0,END)
@@ -24,3 +30,28 @@ def translate(box_in, box_out, lang_in, lang_out):
     temp = trans.translate(input, src=lang_in, dest=lang_out)
     output = temp.text
     box_out.insert(END, output)
+
+def upload_image(box_in):
+    # Mở hộp thoại chọn tệp và lấy đường dẫn tệp
+    file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif")])
+    
+    if file_path:
+        # Mở ảnh bằng PIL        
+        img = Image.open(file_path).convert("RGB")       
+        # Nhận dạng văn bản
+        pixel_values = processor(images=img, return_tensors="pt").pixel_values
+        generated_ids = model.generate(pixel_values)
+        recognized_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+        # # Chuyển đổi ảnh để sử dụng trong Tkinter
+        # img_tk = ImageTk.PhotoImage(img)
+        
+        box_in.delete('1.0', END)  # Xóa nội dung hiện tại trong box_in
+        box_in.insert(END, recognized_text)  # Chèn string tệp ảnh vào box_in
+        # # Tạo hoặc cập nhật Label để hiển thị ảnh
+        # if label.img:
+        #     label.config(image=img_tk)
+        #     label.img = img_tk  # Giữ tham chiếu đến đối tượng ảnh để ngăn bị thu hồi bộ nhớ
+        # else:
+        #     label.img = img_tk
+        #     label.config(image=img_tk)
--- viewer.py
+++ viewer.py
@@ -24,10 +24,10 @@ name.pack(padx=10, pady=10)
 ## Set language
 lang_in = ttk.Combobox(root, values=languages_value, background="#FEEBE7", width=12)
 lang_in.place(relx=0.3, y=75, anchor='center')
-lang_in.set('vietnamese')
+lang_in.set('english')
 lang_out = ttk.Combobox(root, values=languages_value, background="#FEEBE7", width=12)
 lang_out.place(relx=0.7, y=75, anchor='center')
-lang_out.set('english')
+lang_out.set('vietnamese')
 
 ## Input
 box_in = Text(root, width=250, height=7, font=('Arial',15), background='#FEEBE7')
@@ -40,13 +40,17 @@ box_out.pack(padx=50, pady=50)
 ## Button bellow
 button_frame = Frame(root).pack(side='bottom')
 
+upload_button = Button(button_frame, text='UPLOAD IMAGE', font=('ROBOTO',10), bg='#030303', fg='#FFFFFF')
+upload_button.config(command=lambda: upload_image(box_in=box_in))
+upload_button.place(relx=0.7, y=300, anchor='center')
+
 clear_button = Button(button_frame, text='CLEAR', font=('ROBOTO',10), bg='#030303', fg='#FFFFFF')
 clear_button.config(command=lambda: clear(box_in=box_in, box_out=box_out))
-clear_button.place(relx=0.6, y=300, anchor='center')
+clear_button.place(relx=0.5, y=300, anchor='center')
 
 trans_button = Button(button_frame, text='TRANSLATE', font=('ROBOTO',10), bg='#030303', fg='#FFFFFF')
 trans_button.config(command= lambda: translate(box_in=box_in, box_out=box_out, lang_in=languages[lang_in.get()], lang_out=languages[lang_out.get()]))
-trans_button.place(relx=0.4, y=300, anchor='center')
+trans_button.place(relx=0.3, y=300, anchor='center')
 
 
 ## Focus
